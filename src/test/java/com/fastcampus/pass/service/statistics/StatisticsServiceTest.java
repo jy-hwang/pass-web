b5854fa556487749bbacdea23892e2257e44a9fe
jy-hwang <stratojy@gmail.com>
2025-05-20 10:20:58 +0900

    feat. Web - 테스트 작성 2
    1. 통계에 사용하는 차트 데이터를 생성하는 부분을 JUnit 테스트로 생성.
    2. Nested 와 DisplayName 을 사용해서 좀 더 명시적이고 직관적인 테스트 작성.

--- src/test/java/com/fastcampus/pass/service/statistics/StatisticsServiceTest.java
+++ src/test/java/com/fastcampus/pass/service/statistics/StatisticsServiceTest.java
@@ -1,6 +1,8 @@
 package com.fastcampus.pass.service.statistics;
 
 import com.fastcampus.pass.repository.statistics.StatisticsRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,11 +11,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,28 +27,49 @@ public class StatisticsServiceTest {
   @InjectMocks
   private StatisticsService statisticsService;
 
-  @Test
-  public void test_makeChartData() {
-    // given
+  @Nested
+  @DisplayName("통계 데이터를 기반으로 차트 만들기")
+  class MakeChartData {
     final LocalDateTime to = LocalDateTime.of(2025, 5, 19, 0, 0);
 
-    List<AggregatedStatistics> aggregatedStatisticsList = List.of(
-        new AggregatedStatistics(to.minusDays(1), 15, 10, 5),
-        new AggregatedStatistics(to, 10, 8, 2)
-    );
+    @DisplayName("통계 데이터가 있을 때")
+    @Test
+    void makeChartData_when_hasStatistics() {
+      // given
+      List<AggregatedStatistics> aggregatedStatisticsList = List.of(
+          new AggregatedStatistics(to.minusDays(1), 15, 10, 5),
+          new AggregatedStatistics(to, 10, 8, 2)
+      );
 
-    // when
-    when(statisticsRepository.findByStatisticsAtBetweenAndGroupBy(eq(to.minusDays(10)), eq(to))).thenReturn(aggregatedStatisticsList);
-    final ChartData chartData = statisticsService.makeChartData(to);
+      // when
+      when(statisticsRepository.findByStatisticsAtBetweenAndGroupBy(eq(to.minusDays(10)), eq(to))).thenReturn(aggregatedStatisticsList);
+      final ChartData chartData = statisticsService.makeChartData(to);
 
-    // then
-    verify(statisticsRepository, times(1)).findByStatisticsAtBetweenAndGroupBy(eq(to.minusDays(10)), eq(to));
+      // then
+      verify(statisticsRepository, times(1)).findByStatisticsAtBetweenAndGroupBy(eq(to.minusDays(10)), eq(to));
 
-    assertNotNull(chartData);
-    assertEquals(new ArrayList<>(List.of("05-18", "05-19")), chartData.getLabels());
-    assertEquals(new ArrayList<>(List.of(10L, 8L)), chartData.getAttendedCounts());
-    assertEquals(new ArrayList<>(List.of(5L, 2L)), chartData.getCancelledCounts());
+      assertNotNull(chartData);
+      assertEquals(new ArrayList<>(List.of("05-18", "05-19")), chartData.getLabels());
+      assertEquals(new ArrayList<>(List.of(10L, 8L)), chartData.getAttendedCounts());
+      assertEquals(new ArrayList<>(List.of(5L, 2L)), chartData.getCancelledCounts());
+    }
 
+    @DisplayName("통계 데이터가 없을 때")
+    @Test
+    void makeChartData_when_notHasStatistics() {
+      // when
+      when(statisticsRepository.findByStatisticsAtBetweenAndGroupBy(eq(to.minusDays(10)), eq(to))).thenReturn(Collections.emptyList());
+      final ChartData chartData = statisticsService.makeChartData(to);
+
+      // then
+      verify(statisticsRepository, times(1)).findByStatisticsAtBetweenAndGroupBy(eq(to.minusDays(10)), eq(to));
+
+      assertNotNull(chartData);
+      assertTrue(chartData.getLabels().isEmpty());
+
+      assertTrue(chartData.getAttendedCounts().isEmpty());
+      assertTrue(chartData.getCancelledCounts().isEmpty());
+    }
   }
 
 }
